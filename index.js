43499bfe275d9b1779816ec5558622311e4647cd
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-07-04 18:02:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 var name = "Susan"
 var height = 39
-var message = ` is ${height} inches tall` 
+var message = ` is ${height} 39 inches tall` 
 var message = ' is ${name} Susan'
 
 module.exports = {name, height, message}
